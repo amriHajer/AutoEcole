982ab39757d9bce0fc6fdebd7e27b82e326c4d47
amriHajer <hajer.amri93@gmail.com>
2024-10-10 14:52:03 +0200

    test commit

---
+++ src/main/java/com/example/autoecole/repository/TestRepo.java
@@ -0,0 +1,2 @@
+package com.example.autoecole.repository;public interface TestRepo {
+}
--- src/main/java/com/example/autoecole/repository/VehiculeRepo.java
+++ src/main/java/com/example/autoecole/repository/VehiculeRepo.java
@@ -1,6 +1,2 @@
-package com.example.autoecole.repository;
-
-import com.example.autoecole.entity.Vehicule;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface VehiculeRepo extends JpaRepository <Vehicule, Integer> {
+package com.example.autoecole.repository;public class VehiculeRepo {
 }
